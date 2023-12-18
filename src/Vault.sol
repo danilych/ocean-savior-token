1e545ca8ee3859ca7fcad6eab7837868a1da6bee
danilych <danilych45@proton.me>
2023-12-18 01:33:45 +0100

    feat: add withdraw OSR function

--- src/Vault.sol
+++ src/Vault.sol
@@ -9,20 +9,33 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
  * @title Vault
  *
  * @notice An implementation of smart contract to collect and withdraw fees from transfers OceanSavior token. 
- 
  */
 contract Vault is Ownable {
     using SafeERC20 for IERC20;
 
-    // @notice function for withdraw tokens from Vault
-    // @param token address of token which should be withdrawn
-    function withdraw(address token, uint256 amount) external onlyOwner {
-        IERC20(token).safeTransfer(_msgSender(), amount);
+    // @notice OceanSavior token
+    IERC20 token;
+
+    constructor(address token_) {
+        token = IERC20(token_);
+    }
+
+    // @notice function for withdraw OceanSavior tokens from Vault
+    // @param amount value of tokens which sender want to withdraw
+    function withdrawOSR(uint256 amount) external onlyOwner {
+        token.safeTransfer(_msgSender(), amount);
+    }
+
+    // @notice function for withdraw any ERC20 tokens from Vault
+    // @param token_ address of token which should be withdrawn
+    // @param amount value of tokens which sender want to withdraw
+    function withdrawToken(address token_, uint256 amount) external onlyOwner {
+        IERC20(token_).safeTransfer(_msgSender(), amount);
     }
 
     // @notice view function for checking token balance in Vault
-    // @param token address of ERC20 token which balance should be checked
-    function checkBalance(address token) public view returns (uint256){
-        return IERC20(token).balanceOf(address(this));
+    // @param token_ address of ERC20 token which balance should be checked
+    function checkBalance(address token_) public view returns (uint256) {
+        return IERC20(token_).balanceOf(address(this));
     }
 }
